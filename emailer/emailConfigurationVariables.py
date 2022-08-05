9d19bdf358d12e334247284958da051c1170b1a3
Patrick Stephens <patrickcstephens@me.com>
2022-08-05 18:10:27 -0500

    Add files via upload

---
+++ emailer/emailConfigurationVariables.py
@@ -0,0 +1,6 @@
+## Email Sender / Receiver Variables:
+list_of_receiver_email_addresses = ['receiver_email_address']
+
+## Authentication for Email Sender:
+email_username = 'your_email_address'
+email_password = 'your_email_app_password' # The App Password needed after the Google update for Less Secure Apps: https://support.google.com/accounts/answer/6010255?hl=en&p=less-secure-apps&rd=1#zippy=%2Cif-less-secure-app-access-is-on-for-your-account%2Cupdate-your-app-or-operating-system%2Cuse-more-secure-apps%2Cuse-an-app-password
---
+++ emailer/emailDataVariables.py
@@ -0,0 +1,5 @@
+## Import Module Requirements:
+import pandas as pd
+
+example_list = [1, 2, 3]
+df = pd.DataFrame(example_list, columns=['number'])
